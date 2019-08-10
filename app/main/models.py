804972d0eca4a0ed5502f3964de4095d5523a016
Samuel <samsjkang@gmail.com>
2019-08-10 02:15:40 -0400

    cleaned up code according to pep8 guidelines

--- app/main/models.py
+++ app/main/models.py
@@ -1,42 +1,42 @@
 from django.db import models
 from django.contrib.auth.models \
-  import AbstractBaseUser, BaseUserManager, PermissionsMixin
+    import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
 
 class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        '''Creates and saves User with email and password'''
+        if not email:
+            raise ValueError('User did not provide email address')
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-  def create_user(self, email, password=None):
-    '''Creates and saves User with email and password'''
-    if not email:
-      raise ValueError('User did not provide email address')
-    user = self.model(
-      email=self.normalize_email(email),
-    )
-    user.set_password(password)
-    user.save(using=self._db)
-    return user
-
-  def create_superuser(self, email, password):
-    '''Creates a superuser'''
-    user = self.create_user(
-      email,
-      password=password
-    )
-    user.is_admin = True
-    user.save(using=self._db)
-    return user
+    def create_superuser(self, email, password):
+        '''Creates a superuser'''
+        user = self.create_user(
+            email,
+            password=password
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-  '''Custom User model that uses email and password'''
-  email = models.EmailField(
-    verbose_name='email address',
-    max_length=255,
-    unique=True,
-  )
-  name = models.CharField(max_length=255)
-  is_active = models.BooleanField(default=True)
-  is_admin = models.BooleanField(default=False)
+    '''Custom User model that uses email and password'''
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
 
-  objects = UserManager()
+    objects = UserManager()
 
-  USERNAME_FIELD = 'email'
+    USERNAME_FIELD = 'email'
